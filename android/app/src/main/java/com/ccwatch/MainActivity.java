92848e9911e1f3674b643c497be073bf5430ac75
denkeni <denkenie@gmail.com>
2021-11-03 23:13:06 +0800

    Initialize with React Native CLI

---
+++ android/app/src/main/java/com/ccwatch/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ccwatch;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "CCWatch";
+  }
+}
